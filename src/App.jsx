ad53b99d74e481cca476ecc2dec492e73459b567
amananand019 <aman019anand@gmail.com>
2023-07-19 02:40:50 +0530

    Added NavBar and attached NavLink to the pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
 
 function App() {
-  // const x = 23;
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
